06e8b63c91e92e27b6c29672c605ee6f3e20eace
Zhen <angzhenyi@gmail.com>
2024-03-11 01:43:47 +0800

    fix: cors

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -10,7 +10,7 @@ const app = express()
 app.use(express.json())
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'https://tic-tac-toi-frontend.vercel.app/'],
+    origin: ['http://localhost:5173', 'https://tic-tac-toi-frontend.vercel.app'],
   })
 )
 
